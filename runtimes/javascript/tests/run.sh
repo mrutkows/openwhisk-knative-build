099528d96227b01dddef367e066310bee9449e37
Priti Desai <pdesai@us.ibm.com>
2019-01-31 13:10:21 -0800

    adding more tests

--- runtimes/javascript/tests/run.sh
+++ runtimes/javascript/tests/run.sh
@@ -3,3 +3,18 @@
 curl -H "Host: nodejs-10-action.default.example.com" -d "@data-with-simple-hello.json" -H "Content-Type: application/json" http://localhost/init
 
 curl -H "Host: nodejs-10-action.default.example.com" -H "Content-Type: application/json" -X POST http://localhost/run
+
+
+
+curl -H "Host: nodejs-10-action.default.example.com" -d "@data-with-params-hello.json" -H "Content-Type: application/json" http://localhost/init
+
+curl -H "Host: nodejs-10-action.default.example.com" -H "Content-Type: application/json" -X POST http://localhost/run
+
+curl -H "Host: nodejs-10-action.default.example.com" -H "Content-Type: application/json" -d '{ "name": "Amy", "place": "USA" }' http://localhost/run
+
+
+
+curl -H "Host: nodejs-10-action.default.example.com" -d "@data-with-default-params-hello.json" -H "Content-Type: application/json" http://localhost/init
+
+curl -H "Host: nodejs-10-action.default.example.com" -H "Content-Type: application/json" -X POST http://localhost/run
+
